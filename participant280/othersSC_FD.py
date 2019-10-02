10983cc57f6ae674a465fcca8d3b8622bc4085a2
meettanmaysinha <tanmay.sinha655@gmail.com>
2019-10-02 08:28:02 +0200

    remove all pid

--- participant280/othersSC_FD.py
+++
@@ -1,9 +0,0 @@
-def different_idea(task,level):
-
-	if (task=="SC" and level=="FD1"):
-		idea="Scipy has a handy function to generate p-value of an obtained correlation: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.pearsonr.html. If this idea If this idea changes measures you designed earlier or inspires you to create a new measure, try again. In case the idea information is not helpful and you are not sure if/how you might design new measures (or, revise measures you already designed), you can ask for a different idea by calling the function different_idea(\"SC\",\"FD2\")."
-
-	if (task=="SC" and level=="FD2"):
-		idea="Pandas has a handy function to generate correlation table: https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.corr.html. If this idea If this idea changes measures you designed earlier or inspires you to create a new measure, try again."
-		
-	return(idea)
